7982f63a325b2a318742dcdc48c501cda1a0889e
Oliver de Sousa <oliver.seq@gmail.com>
2018-08-25 11:36:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function add2(n) {
 
 function funkyFunction(theFunk) {
   theFunk = "FUNKY!";
-  return funkyFunction;
+  return theFunk;
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
